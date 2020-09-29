33fc43731307abb849318302153704698643374c
skapunker <skapunker@gmail.com>
2020-09-28 18:20:27 -0600

    updates

--- .zshrc
+++ .zshrc
@@ -1,3 +1,10 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 export ZSH=$HOME/.oh-my-zsh
 ZSH_CUSTOM=$HOME/.zsh_custom
 ZSH_THEME="powerlevel10k/powerlevel10k"
@@ -12,3 +19,6 @@ plugins=(
 source $ZSH/oh-my-zsh.sh
 
 if [ -f /usr/bin/screenfetch ]; then screenfetch; fi
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc fonts tmux.conf vim vimrc zshrc zsh_custom"    # list of files/folders to symlink in homedir
+files="bashrc fonts p10k.zsh tmux.conf vim vimrc zshrc zsh_custom"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -26,15 +26,15 @@ for file in $files; do
 done
 
 install_zsh () {
-# Test to see if zshell is installed.  If it is:
+# Test to see if zsh is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/.oh-my-zsh/ ]]; then
-        git clone http://github.com/robbyrussell/oh-my-zsh.git
-    fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
         chsh -s $(which zsh)
+    fi
+    # Install oh-my-zsh if it's missing:
+    if [[ ! -d $dir/.oh-my-zsh/ ]]; then
+        sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     fi
 else
     # If zsh isn't installed, get the platform of the current machine
@@ -49,6 +49,14 @@ else
             sudo apt-get install zsh
             install_zsh
         fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+    # Install oh-my-zsh if it's missing:
+   if [[ ! -d $dir/.oh-my-zsh/ ]]; then
+       sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+   fi
     # If the platform is OS X, tell the user to install zsh :)
     elif [[ $platform == 'Darwin' ]]; then
         echo "Please install zsh, then re-run this script!"
